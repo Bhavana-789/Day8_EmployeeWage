8a788d6790b995345f83ec910b5e92a20b669cc0
Bhavana <bhavananerlakota@gmail.com>
2022-04-05 00:36:31 +0530

    Added UC2 daily employee wage program

--- EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/Program.cs
@@ -7,19 +7,23 @@ namespace EmployeeWageProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Problem.\n");
-            //UC1
-            Console.WriteLine("Welcome to UC1");
+            //UC2
+            Console.WriteLine("Welcome to UC2");
+            int totalWorkingHour = 8;
+            int wagePerHour = 20;
+            int dailyWage = 0;
             Random random = new Random();
             int num = random.Next(0, 2);
             if (num == 0)
             {
-                Console.WriteLine("Employee is present");
+                Console.WriteLine("Employee is Present");
+                dailyWage = totalWorkingHour * wagePerHour;
+                Console.WriteLine("Total Daily wage is:" + dailyWage);
             }
             else
             {
-                Console.WriteLine("Employee is absent");
-
-
+                Console.WriteLine("Employee is Absent");
+                Console.WriteLine("Total daily wage is:" + dailyWage);
             }
         }
     }
