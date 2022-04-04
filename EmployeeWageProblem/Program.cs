d7fd6178810f16d4a1d9dc9a60fb94a654f41044
Bhavana <bhavananerlakota@gmail.com>
2022-04-05 01:54:55 +0530

    Added UC5 MonthlyWage

--- EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/Program.cs
@@ -7,33 +7,40 @@ namespace EmployeeWageProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Problem.\n");
-            //UC4
-            Console.WriteLine("Welcome to UC4");
+            //UC5_MonthlyWage
+            Console.WriteLine("Welcome to UC5");
             int isPartTime = 1;
             int isFullTime = 2;
             int wagePerHour = 20;
             int empHrs;
             int dailywage = 0;
-            Random random = new Random();
-            int EmpWorkingTime = random.Next(0, 3);
-
-            switch (EmpWorkingTime)
+            int totalWorkingHour = 0;
+            int numOfWorkingDays = 20;
+            while (numOfWorkingDays > 0)
             {
-                case 1:
-                    empHrs = 8;
-                    Console.WriteLine("Employee is present for full time");
-                    break;
-                case 2:
-                    empHrs = 4;
-                    Console.WriteLine("Employee is present for part time");
-                    break;
-                default:
-                    empHrs = 0;
-                    Console.WriteLine("Employee is absent");
-                    break;
+                Random random = new Random();
+                int randomCheck = random.Next(0, 3);
+
+                switch (randomCheck)
+                {
+                    case 1:
+                        empHrs = 8;
+                        Console.WriteLine("Employee is present for full time");
+                        break;
+                    case 2:
+                        empHrs = 4;
+                        Console.WriteLine("Employee is present for part time");
+                        break;
+                    default:
+                        empHrs = 0;
+                        Console.WriteLine("Employee is absent");
+                        break;
+                }
+                numOfWorkingDays--;
+                totalWorkingHour = totalWorkingHour + empHrs;
             }
-            dailywage = empHrs * wagePerHour;
-            Console.WriteLine("Total daily wage of employee is:");
+            dailywage = totalWorkingHour * wagePerHour;
+            Console.WriteLine("Total wage is:" + dailywage);
         }
     }
 }
