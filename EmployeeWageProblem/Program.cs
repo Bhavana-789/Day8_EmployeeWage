9560dd22930af988dc7e4d3dc70f322622f038f7
Bhavana <bhavananerlakota@gmail.com>
2022-04-05 00:30:52 +0530

    Added UC1 Attendance Program

--- EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/Program.cs
@@ -7,6 +7,21 @@ namespace EmployeeWageProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Problem.\n");
+            //UC1
+            Console.WriteLine("Welcome to UC1");
+            Random random = new Random();
+            int num = random.Next(0, 2);
+            if (num == 0)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+
+
+            }
         }
     }
 }
+
