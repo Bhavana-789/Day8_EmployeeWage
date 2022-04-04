e538cc9a9bbeda6b69cf5cb23ab3f4328b04cc11
Bhavana <bhavananerlakota@gmail.com>
2022-04-05 00:43:41 +0530

    Added UC3 part time program

--- EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/Program.cs
@@ -7,23 +7,30 @@ namespace EmployeeWageProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Problem.\n");
-            //UC2
-            Console.WriteLine("Welcome to UC2");
+            //UC3
+            Console.WriteLine("Welcome to UC3");
             int totalWorkingHour = 8;
+            int partTimeHour = 4;
             int wagePerHour = 20;
-            int dailyWage = 0;
+            int dailywage = 0;
             Random random = new Random();
-            int num = random.Next(0, 2);
-            if (num == 0)
+            int EmpWorkingTime = random.Next(0, 3);
+            if (EmpWorkingTime == 0)
             {
-                Console.WriteLine("Employee is Present");
-                dailyWage = totalWorkingHour * wagePerHour;
-                Console.WriteLine("Total Daily wage is:" + dailyWage);
+                Console.WriteLine("Employee is present for full time");
+                dailywage = totalWorkingHour * wagePerHour;
+                Console.WriteLine("Total Daily wage is:" + dailywage);
+            }
+            else if (EmpWorkingTime == 1)
+            {
+                Console.WriteLine("Employee is present for part time");
+                dailywage = partTimeHour * wagePerHour;
+                Console.WriteLine("Total Daily Wage is:" + dailywage);
             }
             else
             {
-                Console.WriteLine("Employee is Absent");
-                Console.WriteLine("Total daily wage is:" + dailyWage);
+                Console.WriteLine("Employee is absent");
+                Console.WriteLine("Total Daily Wage is:" + dailywage);
             }
         }
     }
