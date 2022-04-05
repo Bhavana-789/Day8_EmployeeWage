ca6857092217f242bb1dc2b07fa6d91b6effb931
Bhavana <bhavananerlakota@gmail.com>
2022-04-05 09:52:00 +0530

    Added UC7

--- EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/Program.cs
@@ -7,40 +7,9 @@ namespace EmployeeWageProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Problem.\n");
-            //UC6_WagesTillCondition
-            Console.WriteLine("Welcome to UC6");
-            int isPartTime = 1;
-            int isFullTime = 2;
-            int totalWorkingHrs = 0;
-            int wagePerHour = 20;
-            int totalWage = 0;
-            int totalWorkingDays = 1, empHrs;
-            int totalWorkingLimit = 100;
-
-            while (totalWorkingDays <= 20 && totalWorkingHrs <= totalWorkingLimit)
-            {
-                Random random = new Random();
-                int randomCheck = random.Next(0, 3);
-                switch (randomCheck)
-                {
-                    case 1:
-                        empHrs = 8;
-                        Console.WriteLine("Employee is present for full time");
-                        break;
-                    case 2:
-                        empHrs = 4;
-                        Console.WriteLine("Employee is present for part time");
-                        break;
-                    default:
-                        empHrs = 0;
-                        Console.WriteLine("Employee is absent");
-                        break;
-                }
-                totalWorkingDays++;
-                totalWorkingHrs = totalWorkingHrs + empHrs;
-            }
-            totalWage = wagePerHour * totalWorkingHrs;
-            Console.WriteLine("Total wage is:" + totalWage);
+            //UC7_UsingClassMethods
+            Console.WriteLine("Welcome to UC7");
+            UC_7.CalculateEmpWage();
         }
     }
 }
