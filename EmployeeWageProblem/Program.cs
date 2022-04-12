412bf3eab43cdda6508e16197cfe37b220ce4265
Bhavana <bhavananerlakota@gmail.com>
2022-04-12 20:56:42 +0530

    Added UC9 save total wage problem

--- EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/Program.cs
@@ -8,9 +8,18 @@ namespace EmployeeWageProblem
         {
             Console.WriteLine("Welcome to Employee Wage Problem.\n");
             //UC8_MultipleCompanyEmpWage
-            Console.WriteLine("Welcome to UC");
             UC8_MultipleCompanies.CalculateEmpWage("Samsung", 20, 5, 30);
             UC8_MultipleCompanies.CalculateEmpWage("Jio", 10, 4, 20);
+
+
+            //UC9_SaveTotalWages
+            UC9_SaveTotalWage obj = new UC9_SaveTotalWage("Samsung", 20, 100, 20);
+            obj.ComputeEmployeeWage();
+            Console.WriteLine(obj.toString());
+            UC9_SaveTotalWage obj1 = new UC9_SaveTotalWage("Jio", 10, 120, 21);
+            obj1.ComputeEmployeeWage();
+            Console.WriteLine(obj1.toString());
+
         }
     }
 }
